1f683e021cb31ee298c0ce8b802ce8323ccd15ad
elgrande <abaaneg@gmail.com>
2023-01-28 02:23:46 +0000

    fixing error

--- script.js
+++ script.js
@@ -29,14 +29,16 @@ let shopItemsData = [{
     img: "images/img-4.jpg"
 }]
 
-let basket = []
-
+let basket = JSON.parse(localStorage.getItem("data")) || []
+console.log(basket)
 /**
  * ! ES6 error function
  */
 let generateShop = () =>{
-    return (shop.innerHTML = shopItemsData.map((x)=>{
-        let {id, name, price, desc, img} = x 
+    return (shop.innerHTML = shopItemsData.map((x) => {
+        let { id, name, price, desc, img} = x
+        let search = basket.find((x) => x.id === id) || []
+        console.log(search)
         /**
          * ! destructuring
          **/
@@ -50,7 +52,9 @@ let generateShop = () =>{
                 <h2>${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-file-minus-fill"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">
+                    ${search.item === undefined ?  0 : search.item}
+                    </div>
                     <i onclick="increment(${id})" class="bi bi-file-plus-fill"></i>
                 </div>
             </div>
@@ -76,7 +80,7 @@ let increment = (id) => {
         search.item += 1
     }
    
-
+    localStorage.setItem("data", JSON.stringify(basket))
     // console.log(basket)
     update(selectedItem)
 
@@ -91,7 +95,7 @@ let decrement = (id) => {
         search.item -= 1
     }
    
-
+    localStorage.setItem("data", JSON.stringify(basket))
     // console.log(basket)
     update(selectedItem)
 
